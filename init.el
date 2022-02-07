a3dd1939bb7f66ab38ba0af1997e13cb57d33773
mkrupczak3 <matthew@krupczak.org>
2022-02-07 16:09:32 -0500

    Update init.el

--- init.el
+++ init.el
@@ -2,6 +2,8 @@
 (set-face-attribute 'default nil :height 140)
 (setq-default visible-bell 1)
 (setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(setq indent-line-function 'insert-tab)
 (setq-default show-paren-mode 1)
 (if window-system (tool-bar-mode 0))
 (if (boundp 'aquamacs-version) (tabbar-mode 0))
